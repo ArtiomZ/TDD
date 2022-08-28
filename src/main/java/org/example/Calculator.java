11d106458868c308e6c156a04edc79c455ff0c6f
Артём Журавлёв <artemon50309@gmail.com>
2022-08-28 16:45:52 +0200

    initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Calculator {
+    public double monthlyPayment(int creditAmount, int month, double percent) {
+        return 1;
+    }
+
+    public double totalAmount(int creditAmount, int month, double percent) {
+        return 1;
+    }
+
+    public double totalOverpayment(int creditAmount, int month, double percent) {
+        return 1;
+    }
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calc;
+
+    @BeforeEach
+    public void init() {
+        System.out.println("test started");
+        calc = new Calculator();
+    }
+
+    @AfterEach
+    public void finished() {
+        System.out.println("test completed");
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void monthlyPayment() {
+        int creditAmount = 300_000;
+        int month = 18;
+        double percent = 15.0;
+        double expected = 18715.44;
+        double result = calc.monthlyPayment(creditAmount,month,percent);
+        assertEquals(expected,result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void totalAmount() {
+        int creditAmount = 300_000;
+        int month = 18;
+        double percent = 15.0;
+        double expected = 336877.92;
+        double result = calc.totalAmount(creditAmount,month,percent);
+        assertEquals(expected,result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void totalOverpayment() {
+        int creditAmount = 300_000;
+        int month = 18;
+        double percent = 15.0;
+        double expected = 36877.92;
+        double result = calc.totalOverpayment(creditAmount,month,percent);
+        assertEquals(expected,result);
+    }
+}
